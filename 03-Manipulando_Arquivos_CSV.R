82807ad57f6b601699114544b6f986b5fd04eaa8
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-03-29 16:31:31 -0300

    carregando e manipulando arquivos CSV

---
+++ 03-Manipulando_Arquivos_CSV.R
@@ -0,0 +1,71 @@
+# Carregando e Manipulando Arquivos CSV
+
+
+# Configurando Diretório de Trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/5.Manipulacao-de-Arquivos-TXT-CSV-e-Planilhas-Excel-em-R")
+getwd()
+
+
+# Usando o pacote readr
+
+# install.packages('readr')
+library(readr)
+
+
+# Exportando o dataset e gerando o arquivo com o write_csv()
+write_csv(iris, 'iris.csv')
+
+# conteudo do diretorio
+dir()
+
+
+## Tipos de dados
+
+# col_integer()      
+# col_double()
+# col_logical()
+# col_character()
+# col_factor()
+# col_skip()
+# col_date() (alias = "D"), col_datetime() (alias = "T"), col_time() (alias = "t")
+
+
+# Carregando o arquivo com read_csv()
+# (definindo os tipos das colunas - 4 do tipo double e a ultima do tipo fator com um vetor de string para nomes dos fatores)
+
+df_iris <- read_csv("iris.csv",
+                    col_types = list(
+                      Sepal.Length = col_double(),
+                      Sepal.Width = col_double(),
+                      Petal.Length = col_double(),
+                      Petal.Width = col_double(),
+                      Species = col_factor(c('setosa', 'versicolor', 'virginica'))
+                    ))
+View(df_iris)
+dim(df_iris)
+str(df_iris)
+
+
+
+# Usando o pacote utils
+
+# Carregando o arquivo com read.csv() sem parametros
+
+df_iris2 <- read.csv("iris.csv")
+
+View(df_iris2)
+dim(df_iris2)
+str(df_iris2)
+
+
+# Carregando o arquivo com read.csv() com parametro para coluna com string ser do tipo factor
+
+df_iris2 <- read.csv("iris.csv", stringsAsFactors = TRUE)
+
+View(df_iris2)
+str(df_iris2)
+
+
+
+
+
